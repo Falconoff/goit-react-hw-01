44a82cc827829062e8ae227070bf70e598c05484
Falconoff <sokolovserega1982@gmail.com>
2024-09-22 00:29:29 +0300

    init: new proj, add: 1-st Task

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,33 @@
+// import React from 'react';
+import styles from './Profile.module.css';
+
+// console.log(styles);
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className={styles.profileCard}>
+      <div className={styles.userData}>
+        <img className={styles.avatar} src={image} alt="User avatar" />
+        <p className={styles.userName}>{name}</p>
+        <p className={styles.userTag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+      <ul className={styles.stats}>
+        <li className={styles.statsItem}>
+          <span className="stats-header">Followers</span>
+          <span className={styles.statsData}>{stats.followers}</span>
+        </li>
+        <li className={styles.statsItem}>
+          <span className="stats-header">Views</span>
+          <span className={styles.statsData}>{stats.views}</span>
+        </li>
+        <li className={styles.statsItem}>
+          <span className="stats-header">Likes</span>
+          <span className={styles.statsData}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
